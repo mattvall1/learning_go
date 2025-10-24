5afabdea3e33adedb0006886cd224d6f5839d703
Matthew Vallance <matthew.vallance@outlook.com>
2025-10-24 15:10:01 +0100

    Create 4_advanced_calculator.go

---
+++ lln_course/code_challenges/4_advanced_calculator.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	value1 := "10"
+	value2 := "5.5"
+	operation := "+"
+	result := calculate(value1, value2, operation)
+	fmt.Println(result)
+}
+
+// calculate() returns the result of the requested operation.
+func calculate(input1 string, input2 string, operation string) float64 {
+	var result float64
+	flt1 := convertInputToValue(input1)
+	flt2 := convertInputToValue(input2)
+	switch {
+	case operation == "+":
+		result = addValues(flt1, flt2)
+	case operation == "-":
+		result = subtractValues(flt1, flt2)
+	case operation == "*":
+		result = multiplyValues(flt1, flt2)
+	case operation == "/":
+		result = divideValues(flt1, flt2)
+	default:
+		result = 0
+	}
+
+	return result
+}
+
+func convertInputToValue(input string) float64 {
+	float, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+	return float
+}
+
+func addValues(value1, value2 float64) float64 {
+	return value1 + value2
+}
+
+func subtractValues(value1, value2 float64) float64 {
+	return value1 - value2
+}
+
+func multiplyValues(value1, value2 float64) float64 {
+	return value1 * value2
+}
+
+func divideValues(value1, value2 float64) float64 {
+	return value1 / value2
+}
